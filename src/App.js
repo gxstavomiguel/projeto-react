a786f33fa9152fb928b2f1f56061ce2201449580
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-08-04 18:07:37 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from 'react';
 
 function App() {
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [resultado, setResultado] = useState('');
+
+  function handleClick() {
+    console.log(input1, input2);
+    if (input1 < input2) {
+      setResultado(input1);
+    } else {
+      setResultado(input2);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <input 
+          className="input1"
+          value={input1}
+          onChange={(e) => setInput1(e.target.value)}
+          placeholder="Enter a number"
+        />
+
+        <input 
+          className="input2"
+          value={input2}
+          onChange={(e) => setInput2(e.target.value)}
+          placeholder="Enter another number"
+        />
+
+        <div>
+          <p>Resultado: {resultado}</p>
+        </div>
+
+        <button onClick={handleClick}>Calcular</button>
+      </div>
     </div>
   );
 }
